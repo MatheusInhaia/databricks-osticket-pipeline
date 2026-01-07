9078239e56f867b27f98268a7fad65e44b2549af
MatheusInhaia <matheus.inhaia2014@gmail.com>
2026-01-07 14:17:29 +0000

    refactor: replace %sql with spark.sql

--- OSTicket/Pipeline/Gold/kpi_sla_mensal.py
+++ OSTicket/Pipeline/Gold/kpi_sla_mensal.py
@@ -1,38 +1,33 @@
 # Databricks notebook source
-# MAGIC %sql
-# MAGIC create or replace table osticket.gold.kpi_sla_mensal as
-# MAGIC select
-# MAGIC     year(created)  as year,
-# MAGIC     month(created) as month,
-# MAGIC     (year(created) * 100 + month(created)) as year_month,
-# MAGIC
-# MAGIC     count(staff_first_resp) as total_tickets,
-# MAGIC
-# MAGIC     sum(
-# MAGIC         case when resp_time <= 1440 then 1 else 0 end
-# MAGIC     ) as sla_24h,
-# MAGIC
-# MAGIC     round(
-# MAGIC         sla_24h
-# MAGIC         / total_tickets,
-# MAGIC         4
-# MAGIC     ) as perc_sla_24h
-# MAGIC
-# MAGIC from osticket.silver.thread_entry_agg
-# MAGIC group by
-# MAGIC     year(created),
-# MAGIC     month(created),
-# MAGIC     year_month
-# MAGIC order by
-# MAGIC     year(created),
-# MAGIC     month(created),
-# MAGIC     year_month;
-# MAGIC
+spark.sql("""
+            create or replace table osticket.gold.kpi_sla_mensal as
+            select
+                year(created)  as year,
+                month(created) as month,
+                (year(created) * 100 + month(created)) as year_month,
 
-# COMMAND ----------
+                count(staff_first_resp) as total_tickets,
 
-# MAGIC %sql
-# MAGIC select * from osticket.gold.kpi_sla_mensal
+                sum(
+                    case when resp_time <= 1440 then 1 else 0 end
+                ) as sla_24h,
+
+                round(
+                    sla_24h
+                    / total_tickets,
+                    4
+                ) as perc_sla_24h
+
+            from osticket.silver.thread_entry_agg
+            group by
+                year(created),
+                month(created),
+                year_month
+            order by
+                year(created),
+                month(created),
+                year_month;
+          """)
 
 # COMMAND ----------
 
